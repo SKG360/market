8586f54e5dcb36b0a07cfb05215cb1f91b997b0f
SKG360 <samuel.giles@colorado.edu>
2018-07-31 11:41:35 -0600

    Adds tests thru Iteration Two skips Peaches test

---
+++ lib/market.rb
@@ -0,0 +1,32 @@
+require 'pry'
+class Market
+
+  attr_reader :name, :vendors
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def vendor_names
+    names = @vendors.map do |vendor|
+      vendor.name
+    end
+    names
+  end
+
+  def vendors_that_sell(peaches)
+    peach_vendors = @vendors.map do |vendor|
+      vendor.inventory.each_with_index(peaches)
+      # binding.pry
+    end
+    peach_vendors
+  end
+
+  def add_vendor(vendor_objects)
+    @vendors << vendor_objects
+  end
+
+  def sorted_item_list
+    
+  end
+end
---
+++ test/market_test.rb
@@ -0,0 +1,111 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+require './lib/market'
+
+
+class MarketTest < Minitest::Test
+  def test_if_it_exists
+    market = Market.new("South Pearl Street Farmers Market")
+    assert_instance_of Market, market
+  end
+
+  def test_the_vendor_name
+    market = Market.new("South Pearl Street Farmers Market")
+
+    assert_equal "South Pearl Street Farmers Market", market.name
+  end
+
+  def test_if_the_vender_arrary_is_empty
+    market = Market.new("South Pearl Street Farmers Market")
+
+    assert_equal [], market.vendors
+  end
+
+  def test_for_first_vendor
+    market = Market.new("South Pearl Street Farmers Market")
+
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+
+    assert_equal [vendor_1, vendor_2, vendor_3], market.vendors
+  end
+
+  def test_for_array_of_vendors
+    market = Market.new("South Pearl Street Farmers Market")
+
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+
+    assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], market.vendor_names
+  end
+
+  def test_for_peach_sellers
+    skip
+    market = Market.new("South Pearl Street Farmers Market")
+
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+
+    assert_equal [vendor_1, vendor_3], market.vendors_that_sell("Peaches")
+  end
+
+  def test_for_peach_sellers
+
+    market = Market.new("South Pearl Street Farmers Market")
+
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+
+    assert_equal ["Banana Nice Cream", "Peach-Raspberry Nice Cream", "Peaches", "Tomatoes"], market.vendors_that_sell("Peaches")
+  end
+
+end
